3b90b40f5fcb69c7e6fad0d4d199514ec75dde6c
Alessandra <cb.alessandra@gmail.com>
2022-09-14 13:46:25 -0300

    desafiospython25

--- Mundo_3/desafio85.py
+++ Mundo_3/desafio85.py
@@ -4,3 +4,16 @@
 # NO FINAL, MOSTRE OS VALORES PARES E ÍMPARES EM ORDEM CRESCENTE.
 
 print('\n\033[34m====== DESAFIO 85 ======\n\033[m')
+
+numeros = [[],[]]
+
+for n in range(1,8):
+    num = int(input(f'Digite o {n}º valor: '))
+    if num % 2 == 0:
+        numeros[0].append(num)
+    else:
+        numeros[1].append(num)
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os valores pares digitados foram : {numeros[0]}')
+print(f'Os valores ímpares digitados foram : {numeros[1]}')
