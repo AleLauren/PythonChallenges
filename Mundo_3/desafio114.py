fc5db3f7b8fa37812d606e64a16b00aa3d543b42
Alessandra <cb.alessandra@gmail.com>
2022-10-07 00:04:52 -0300

    desafiospython44

--- Mundo_3/desafio114.py
+++ Mundo_3/desafio114.py
@@ -2,3 +2,23 @@
 # CRIE UM CÓDIGO EM PYTHON QUE TESTE SE O SITE PUDIM ESTÁ ACESSÍVEL PELO COMPUTADOR USADO.
 
 print('\n\033[34m====== DESAFIO 114 ======\n\033[m')
+
+import urllib.request
+
+try:
+    pudim=urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.request.URLError:
+    print('O site PUDIM está acessível no momento.')
+else:
+    print('O site PUDIM está acessível!')
+# PARA LER O CÓDIGO DO SITE
+#    print(pudim.read())
+
+# PARA VERIFICAR SE QUALQUER SITE ESTÁ DISPONÍVEL:
+site=str(input('Deseja verificar outro site? Insira o site: '))
+try:
+    site1=urllib.request.urlopen('http://www.'+site+'/')
+except urllib.request.URLError:
+    print('O site não está acessível no momento.')
+else:
+    print('O site está acessível!')
